f640f1d23e0baf8483930c5b9f96454e71b43e1f
akhilesh-vs <118534271+akhilesh-vs@users.noreply.github.com>
2023-06-04 23:49:33 +0530

    Final Commit

--- Upgrad_Project/scripts/header.js
+++ Upgrad_Project/scripts/header.js
@@ -4,9 +4,7 @@ var span = document.getElementsByClassName("close")[0];
 var modal1 = document.getElementById("myModal1");
 var btn1 = document.getElementById("myBtn1");
 var span1 = document.getElementsByClassName("close")[1];
-var modal2 = document.getElementById("myModal2");
-var btn2= document.getElementById("myBtn2");
-var span2 = document.getElementsByClassName("close")[2];
+
 var count=0;
 btn.onclick = function() {
   modal.style.display = "block";
@@ -14,6 +12,7 @@ btn.onclick = function() {
 }
 span.onclick = function() {
   modal.style.display = "none";
+  count=0;
 }
 window.onclick = function(event) 
 {
@@ -22,33 +21,41 @@ window.onclick = function(event)
   }
   else if(event.target == modal1 && count==0) {
     modal1.style.display = "none";
+    count=0;
   }
-  else if(event.target == modal2 && count==3) 
+  else if(event.target == modal3 && count==3) 
   {
-    modal2.style.display = "none";
+    modal3.style.display = "none";
+    count=0;
   }
 }
 btn1.onclick = function() {
   modal1.style.display = "block";
   count=0;
 }
-
 span1.onclick = function() {
   modal1.style.display = "none";
 }
-
-btn2.onclick = function() {
-  modal2.style.display = "block";
-  count=3;
-}
-span2.onclick = function() 
-{
-  modal2.style.display = "none";
-}
 myBtn13.onclick = function() {
-  modal1.style.display = "none";
   modal.style.display = "block";
+  modal1.style.display = "none";
   count=1;
 }
+
+
+
+var modal3 = document.getElementById("myModal3");
+var btn3 = document.getElementById("myBtn3");
+var span3 = document.getElementsByClassName("close")[2];
+ function func() {
+  modal3.style.display = "block";
+  count=3;
+}
+span3.onclick = function() {
+  modal3.style.display = "none";
+  count=0;
+}
+
+
 
 
